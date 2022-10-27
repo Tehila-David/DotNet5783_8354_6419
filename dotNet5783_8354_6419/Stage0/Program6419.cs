a1c95851cf516695adadc85632137bda1ef0ad94
Tehila859 <tedavid@g.jct.ac.il>
2022-10-27 23:43:37 +0300

    initial code + welcome functions

---
+++ dotNet5783_8354_6419/Stage0/Program6419.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome6419();
+            welcome8354();
+            Console.ReadKey();
+        }
+        static partial void welcome8354(); 
+        private static void welcome6419()
+        {
+            Console.WriteLine("Enter your Name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("{0} Welcome to my first console application", username);
+        }
+    }
+}
