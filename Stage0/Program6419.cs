ce5702dadc1c2ede3b6c19b97399ef92b363a830
Tehila David <tedavid@g.jct.ac.il>
2022-11-01 23:09:34 +0200

    commit beishur shel eliezer

---
+++ Stage0/Program6419.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static int Main (string[]args)
+        {
+            Console.WriteLine("Enter your name:");
+            string  username = Console.ReadLine();
+            Console.WriteLine($"{username}, welcome to my first Console application");
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
