b70cd675c9a3e9c4891745cd61b83777baeb3fd4
Ayala Gellis <agellis@g.jct.ac.il>
2022-11-04 17:10:36 +0200

    struct

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace DO
+namespace DO;
+
+internal struct OrderItem
 {
-    internal class OrderItem
-    {
-    }
 }
