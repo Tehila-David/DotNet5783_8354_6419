21bd263cf02bc78f98d7eed4e319179f1dffe386
Tehila David <tedavid@g.jct.ac.il>
2022-11-11 16:08:32 +0200

    array

--- DalList/DalOrder.cs
+++ DalList/DalOrder.cs
@@ -4,4 +4,5 @@ namespace Dal;
 
 public class DalOrder
 {
+    void add();
 }
--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -1,7 +1,11 @@
-﻿namespace DalList;
+﻿
 
-static  internal class DataSource
+namespace Dal;
+
+static internal class DataSource
 {
-    readonly;
-        internal
+
+    internal static DalProduct[] products = new DalProduct[50]; 
+    internal static DalOrderItem[] orderItem = new DalOrderItem[100];
+    internal static DalOrder[] order = new DalOrder[200];
 }
