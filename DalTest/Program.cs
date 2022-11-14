044e13ef91c4a2cfbf9c4007dd7a2da684360979
Tehila David <tedavid@g.jct.ac.il>
2022-11-15 00:14:49 +0200

    NOT work: main=> category, print array

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -10,7 +10,7 @@ class Program
         //DalProduct product;
         int choice;
         Console.WriteLine(" Input: O - Exit , 1 - Product , 2 - Order, 3 - Order Item");
-        choice = Console.Read();
+        choice = int.Parse(Console.ReadLine());
         while (choice != 0)
         {
             try
@@ -41,7 +41,7 @@ class Program
             }
 
             Console.WriteLine(" Input : O - Exit , 1 - Product , 2 - Order, 3 - Order Item");
-            choice = Console.Read();
+            choice = int.Parse(Console.ReadLine());
         }
         return;
     }
@@ -49,7 +49,7 @@ class Program
     {
         Console.WriteLine(" Input : a - Add a product, b - Show a product by ID, c -  Show an array of products, d- Update a product , e - Delete a product");
         char choice;
-        choice = (char)Console.Read();
+        choice = char.Parse(Console.ReadLine());
         switch (choice)
         {
             case 'a': ///add product
@@ -57,18 +57,18 @@ class Program
                 Console.WriteLine("Enter product name:");
                 product.Name = Console.ReadLine();
                 Console.WriteLine("Enter product Category:");
-                product.Category = (Category)Console.Read();
+                product.Category = Category.Parse(Console.ReadLine());
                 Console.WriteLine("Enter product Price:");
-                product.Price = Console.Read();
+                product.Price = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter product amount:");
-                product.InStock = Console.Read();
+                product.InStock = int.Parse(Console.ReadLine());
                 int id;
                 id= DalProduct.addProduct(product);
                 break;
 
             case 'b': ///show single product
                 Console.WriteLine("Enter product ID:");
-                int idForItem = Console.Read();
+                int idForItem = int.Parse(Console.ReadLine());
                 Product singleProduct;
                 singleProduct = DalProduct.getSingleProduct(idForItem);
                 Console.WriteLine(singleProduct);
@@ -83,21 +83,21 @@ class Program
             case 'd':
                 Product product2 = new Product();
                 Console.WriteLine("Enter ID: ");
-                product2.ID= Console.Read();
+                product2.ID= int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter product name:");
                 product2.Name = Console.ReadLine();
                 Console.WriteLine("Enter product Category:");
                 product2.Category = (Category)Console.Read();
                 Console.WriteLine("Enter product Price:");
-                product2.Price = Console.Read();
+                product2.Price = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter product amount:");
-                product2.InStock = Console.Read();
+                product2.InStock = int.Parse(Console.ReadLine());
 
                 DalProduct.updateProduct(product2);
                 break;
             case 'e':
                 Console.WriteLine("Enter ID: ");
-                int delID= Console.Read();
+                int delID= int.Parse(Console.ReadLine());
                 DalProduct.deleteProduct(delID);
                 break;
             default:
@@ -109,7 +109,7 @@ class Program
     {
         Console.WriteLine(" Input : a - Add a order, b - Show a order by ID, c -  Show an array of orders, d- Update a order , e - Delete a order");
         char choice;
-        choice = (char)Console.Read();
+        choice = char.Parse(Console.ReadLine());
         switch (choice)
         {
             case 'a': ///add product
@@ -128,7 +128,7 @@ class Program
 
             case 'b': ///show single order
                 Console.WriteLine("Enter order ID:");
-                int idForItem1 = Console.Read();
+                int idForItem1 = int.Parse(Console.ReadLine());
                 Order singleOrder;
                 singleOrder = DalOrder.getSingleOrder(idForItem1);
                 Console.WriteLine(singleOrder);
@@ -157,7 +157,7 @@ class Program
                 break;
             case 'e':
                 Console.WriteLine("Enter ID: ");
-                int delID1 = Console.Read();
+                int delID1 = int.Parse(Console.ReadLine());
                 DalOrder.deleteOrder(delID1);
                 break;
             default:
@@ -175,20 +175,20 @@ class Program
             case 'a': ///add product
                 OrderItem orderItem = new OrderItem();
                 Console.WriteLine("Enter Product ID:");
-                orderItem.ProductID = Console.Read();
+                orderItem.ProductID = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter Order ID:");
-                orderItem.OrderID = Console.Read();
+                orderItem.OrderID = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter  Price:");
-                orderItem.Price = Console.Read();
+                orderItem.Price = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter  amount:");
-                orderItem.Amount = Console.Read();
+                orderItem.Amount = int.Parse(Console.ReadLine());
                 int id;
                 id = DalOrderItem.addOrderItem(orderItem);
                 break;
 
             case 'b': ///show single order item
                 Console.WriteLine("Enter order item ID:");
-                int idForItem3 = Console.Read();
+                int idForItem3 = int.Parse(Console.ReadLine());
                 OrderItem singleOrderItem;
                 singleOrderItem = DalOrderItem.getSingleOrderItem(idForItem3);
                 Console.WriteLine(singleOrderItem);
@@ -203,20 +203,20 @@ class Program
             case 'd':
                OrderItem orderItem2 = new OrderItem();
                 Console.WriteLine("Enter ID: ");
-                orderItem2.ID = Console.Read();
+                orderItem2.ID = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter Product ID:");
-                orderItem2.ProductID = Console.Read();
+                orderItem2.ProductID = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter Order ID:");
-                orderItem2.OrderID = Console.Read();
+                orderItem2.OrderID = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter  Price:");
-                orderItem2.Price = Console.Read();
+                orderItem2.Price = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter  amount:");
-                orderItem2.Amount = Console.Read();
+                orderItem2.Amount = int.Parse(Console.ReadLine());
                 DalOrderItem.updateOrderItem(orderItem2);
                 break;
             case 'e':
                 Console.WriteLine("Enter ID: ");
-                int delID3 = Console.Read();
+                int delID3 = int.Parse(Console.ReadLine());
                 DalOrderItem.deleteOrderItem(delID3);
                 break;
             default:
