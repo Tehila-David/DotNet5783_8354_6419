e67ac3a1ccce74656deae34f9cfdf30a2727df75
Tehila David <tedavid@g.jct.ac.il>
2022-11-22 13:00:34 +0200

    KK

--- DalFacade/DalApi/ICrud.cs
+++ DalFacade/DalApi/ICrud.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DO;
 
 namespace DalApi
 {
-    internal interface ICrud
+    public interface ICrud
     {
+        int ADD(
     }
 }
