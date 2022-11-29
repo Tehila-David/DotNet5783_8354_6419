e279dc57f3ce30be9bdfee8678c26e3f919858b9
Tehila David <tedavid@g.jct.ac.il>
2022-11-29 13:31:29 +0200

    11

--- BL/BlApi/Icart.cs
+++ BL/BlApi/Icart.cs
@@ -7,9 +7,15 @@ using System.Threading.Tasks;
 
 namespace BlApi
 {
-    public interface ICart
+    public interface IOrderItem
     {
-       
+        public int Add(BO.OrderItem myOrderItem);
+        public BO.OrderItem GetById(int id);
+        public IEnumerable<BO.OrderItem> GetAll();
+        public void Delete(int ID);
+        public void Update(BO.OrderItem myOrderItem);
+        public BO.OrderItem getOrderItemBasedOnProducIDAndOrderID(int idOrder, int idProduct);
+        public IEnumerable<BO.OrderItem> getListOfOrderItemsBasedOnOrderID(int idOrder);
 
     }
 }
