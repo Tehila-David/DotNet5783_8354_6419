f4a9eaa801319afd299e8c97729612cca3b33886
Ayala Gellis <agellis@g.jct.ac.il>
2022-11-29 13:36:15 +0200

    ckhl

--- BL/BlImplementation/Product.cs
+++ BL/BlImplementation/Product.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlApi;
+using BO;
+namespace BlImplementation;
 
-namespace BlImplementation
+internal class Product: IProduct
 {
-    internal class Product
-    {
-    }
+
 }
