0f383a34937e794d8cd434ddba9793dfc3f3a5e8
Ayala Gellis <agellis@g.jct.ac.il>
2022-11-29 13:23:32 +0200

    hi

--- BL/BlApi/ICart.cs
+++ BL/BlApi/ICart.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BlApi
 {
-    public interface IOrderItem
+    public interface ICart
     {
         public int Add(BO.OrderItem myOrderItem);
         public BO.OrderItem GetById(int id);
