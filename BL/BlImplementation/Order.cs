4c24587cb9487dbe965acf0218bbd5868df1daa6
Tehila David <tedavid@g.jct.ac.il>
2022-12-06 17:48:44 +0200

    gg

--- BL/BlImplementation/Order.cs
+++ BL/BlImplementation/Order.cs
@@ -7,6 +7,7 @@ using BlApi;
 using DalApi;
 
 using Dal;
+using BO;
 
 
 namespace BlImplementation;
@@ -30,16 +31,66 @@ internal class Order: BlApi.IOrder
     }
     public BO.Order GetByID(int id)
     {
-
+        try
+        {
+            DO.Order order = Dal?.Order.GetById(id) ?? throw new DO.NotExists("Got null order from data");
+            BO.OrderStatus status = OrderStatus.Confirmed;
+            if (order.ShipDate != DateTime.MinValue) { status = BO.OrderStatus.shipped; }
+            if (order.DeliveryDate != DateTime.MinValue) { status = BO.OrderStatus.Deliverded; }
+            if (id < 0) { throw new BO.InternalProblem("ID not positive"); }
+            return new BO.Order()
+            {
+                ID = order.ID,
+                CustomerName = order.CustomerName,
+                CustomerEmail= order.CustomerEmail,
+                CustomerAddress= order.CustomerAddress,
+                Status= BO.OrderStatus.Confirmed,///!!!!!!!!!!!!!!!
+                OrderDate = order.OrderDate,
+            };
+        }
+        catch (DO.NotExists ex)
+        {
+            throw new BO.InternalProblem("Sorry ,this order does not exist in the List ", ex);
+        }
     }
-    
+
     public BO.Order UpdateDelivery(int id)
+    {
+        try
+        {
+            if (id < 0) { throw new BO.InternalProblem("ID not positive"); }
+            DO.Order order = Dal?.Order.GetById(id) ?? throw new DO.NotExists("Got null order from data");
+            if (order.DeliveryDate != DateTime.MinValue) { throw new BO.InternalProblem("The order shipped"); }
+            order.DeliveryDate = DateTime.Now;
+            Dal.Order.Update(order);
+            return new BO.Order()
+            {
+                ID = order.ID,
+                CustomerName = order.CustomerName,
+                CustomerEmail = order.CustomerEmail,
+                CustomerAddress = order.CustomerAddress,
+                Status = BO.OrderStatus.Confirmed,///!!!!!!!!!!!!!!!
+                OrderDate = order.OrderDate,
+                DeliveryDate = order.DeliveryDate,
+                ShipDate= order.ShipDate,
+                TotalPrice=
+
+            };
+        }
+        catch (DO.NotExists ex)
+        {
+            throw new BO.InternalProblem("Sorry ,this order does not exist in the List ", ex);
+        }
+    }
+
+    public BO.Order UpdateShipDate(int id)
     {
 
     }
-    
-    public BO.Order UpdateShipDate(int id);
    
-    public BO.Order followOrder(int id);
+    public BO.Order followOrder(int id)
+    {
+
+    }
 
 }
