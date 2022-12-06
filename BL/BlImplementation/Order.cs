fc2a29d0479f000e3cc32fb690113ba177e56bc4
Tehila David <tedavid@g.jct.ac.il>
2022-12-06 15:04:39 +0200

    change

--- BL/BlImplementation/Order.cs
+++ BL/BlImplementation/Order.cs
@@ -8,10 +8,38 @@ using DalApi;
 
 using Dal;
 
+
 namespace BlImplementation;
 
 internal class Order: BlApi.IOrder
 {
     IDal Dal = new DalList();
+
+
+    public IEnumerable<BO.OrderForList> GetListedOrders()
+    {
+        return Dal.Order.GetAll().Select(order => new BO.OrderForList
+        {
+
+            ID = order.ID,
+            CustomerName = order.CustomerName,
+            Status = BO.OrderStatus.Confirmed,
+            AmountOfItems = Dal.OrderItem.getListOrderItems(order.ID).Sum(orderItem => orderItem.Amount),
+            TotalPrice = (Dal.OrderItem.getListOrderItems(order.ID).Sum(orderItem => orderItem.Price * orderItem.Amount))
+        });
+    }
+    public BO.Order GetByID(int id)
+    {
+
+    }
+    
+    public BO.Order UpdateDelivery(int id)
+    {
+
+    }
+    
+    public BO.Order UpdateShipDate(int id);
+   
+    public BO.Order followOrder(int id);
 
 }
