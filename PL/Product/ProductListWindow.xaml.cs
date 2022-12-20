9f7f5460db75e79be004095a51333201d1484bc2
Tehila David <tedavid@g.jct.ac.il>
2022-12-20 17:04:40 +0200

    ss

--- PL/Product/ProductListWindow.xaml.cs
+++ PL/Product/ProductListWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace PL
            ProductsList.ItemsSource=bl.Product.GetListedProducts();
            CategorySelector.ItemsSource= Enum.GetValues(typeof(BO.Category));
            CategorySelector.SelectedItem= Enum.GetValues(typeof(BO.Category));
-           CategorySelector.ItemsSource = /*bl.Product(CategorySelector.SelectedItem);*/
+        //   CategorySelector.ItemsSource = /*bl.Product(CategorySelector.SelectedItem);*/
         }
 
         private void ProductsList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
