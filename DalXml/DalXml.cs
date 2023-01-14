4becb240f017107d46f6ffadf7fc1c0cdd661a20
Tehila David <tedavid@g.jct.ac.il>
2023-01-15 00:45:24 +0200

    we Have to hurry up!!!

---
+++ DalXml/DalXml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using DalApi;
+using DO;
+
+namespace Dal
+{
+    internal sealed class DalXml : IDal
+    {
+        public static IDal Instance { get; } = new DalXml();
+        public IOrder Order { get; }
+        public IProduct Product { get; }
+        public IOrderItem OrderItem { get; }
+    }
+}
